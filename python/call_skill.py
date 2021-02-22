88d0686351ad63d995dc9f2320434799374e108d
Rctue <Rctue@users.noreply.github.com>
2021-02-22 13:59:42 +0100

    call misty skill from python
    This calls the eye_contact skill using the triggerevent RESTful api

---
+++ python/call_skill.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 12:16:49 2021
+
+@author: raymond
+"""
+import requests
+import json
+
+robot_ip = '192.168.178.66'
+skill_id = "5d3e55e9-c878-4fbc-8d62-172fbdd9c48c"
+
+def send_command(the_command):
+    resp = requests.post('http://'+robot_ip+'/api/skills/event',json={
+        "Skill" : skill_id,
+        "EventName": "guardian",
+        "Payload": {"guardian_command": "eye_contact",
+                    "guardian_data": the_command},
+        "Source": "MyRobotApplication"})
+    return resp
+
+s=""
+while not s=="quit":
+    s=input("Give a command or type quit: ")
+    resp = send_command(s)
+    print(resp.json())
+
--- python/mistylib.py
+++ python/mistylib.py
@@ -1,3 +1,11 @@
 import requests
 import json
+import mistyPy.mistyPy as mistyPy
+
+if __name__=="__main__":
+    # TODO: Replace with your IP
+    misty = mistyPy.Robot("192.168.178.66") # This is the IP of my misty. Replace with your IP
+    misty.changeLED(0, 0, 255)
+    misty.moveHeadPosition(0, 0, 0, 100) # center the head
+    misty.moveArmsDegrees(0, 0, 100, 100)
 
