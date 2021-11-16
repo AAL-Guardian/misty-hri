84cf6b613a47695531022f0d3428747c7d867415
ilariaciuffreda <71693337+ilariaciuffreda@users.noreply.github.com>
2021-11-16 18:41:02 +0100

    Add files via upload

--- skills/change_led/Change_led.js
+++ skills/change_led/Change_led.js
@@ -2,5 +2,9 @@ misty.RegisterUserEvent("change_led", true);
 misty.Debug("Ho inziato la skill")
 function _change_led() {
     misty.ChangeLED(155,244,4); 
-    misty.DisplayImage("e_Joy2.jpg",4); 
+    misty.DisplayImage("e_Joy2.jpg",4);
+    misty.Pause(2000);
+    misty.ChangeLED(0,0,225); ù
+    misty.DisplayImage("e_DefaultContent.jpg",4);
+    
 }
