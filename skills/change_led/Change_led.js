fadf889000e048676ca67ccbcbc7a67f4eb608a2
ilariaciuffreda <71693337+ilariaciuffreda@users.noreply.github.com>
2021-11-22 09:26:14 +0100

    Update Change_led.js

--- skills/change_led/Change_led.js
+++ skills/change_led/Change_led.js
@@ -4,7 +4,7 @@ function _change_led() {
     misty.ChangeLED(155,244,4); 
     misty.DisplayImage("e_Joy2.jpg",4);
     misty.Pause(2000);
-    misty.ChangeLED(0,0,225); ù
+    misty.ChangeLED(0,0,225); 
     misty.DisplayImage("e_DefaultContent.jpg",4);
     
 }
