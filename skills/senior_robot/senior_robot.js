386a98ac38e779214c240ade03d08dc98e357f9c
ilariaciuffreda <71693337+ilariaciuffreda@users.noreply.github.com>
2022-05-31 10:38:34 +0200

    Add files via upload

--- skills/senior_robot/senior_robot.js
+++ skills/senior_robot/senior_robot.js
@@ -1,20 +1,22 @@
-misty.RegisterUserEvent("how_are_you", true);
+misty.RegisterUserEvent("emotion_how_are_you", true);
 
 // TimerEvent callback
-function _how_are_you(data) {
+function _emotion_how_are_you(data) {
 
     // INPUT PARAMETERS
     data = JSON.parse(data.guardian_data);
     misty.MoveHead(0, 40, 0, 100);
+    misty.Pause(3000);
+    misty.MoveHead(0, 0, 0, 100);
 }
 
 
-misty.RegisterUserEvent("yes", true);
+misty.RegisterUserEvent("emotion_yes", true);
 
 // TimerEvent callback
-function _yes(data) {
+function _emotion_yes(data) {
 
-    // INPUT PARAMETERS
+    // INPUT PARAMETER
     data = JSON.parse(data.guardian_data);
     misty.MoveHead(-40, 0, 0, 100);
     misty.MoveHead(26, 0, 0, 100);
@@ -26,8 +28,8 @@ function _yes(data) {
 } 
 
 
-misty.RegisterUserEvent("medication", true);
-function _medication(data) {
+misty.RegisterUserEvent("emotion_medication", true);
+function _emotion_medication(data) {
 
     // INPUT PARAMETERS
     misty.MoveHead(0, 0, 0, 100);
@@ -36,10 +38,10 @@ function _medication(data) {
     misty.DisplayImage("e_DefaultContent.jpg");
 
 } 
-misty.RegisterUserEvent("yesmedication", true);
+misty.RegisterUserEvent("emotion_yesmedication", true);
 
 // TimerEvent callback
-function _yesmedication(data) {
+function _emotion_yesmedication(data) {
 
     // INPUT PARAMETERS
     data = JSON.parse(data.guardian_data);
@@ -52,8 +54,8 @@ function _yesmedication(data) {
 
 } 
 
-misty.RegisterUserEvent("meal", true);
-function _meal(data) {
+misty.RegisterUserEvent("emotion_meal", true);
+function _emotion_meal(data) {
 
     misty.DisplayImage("e_EcstacyStarryEyed.jpg");
     misty.Pause(3000);
@@ -63,8 +65,8 @@ function _meal(data) {
     misty.MoveArmsDegrees(0, 0, 100, 100);
 } 
 
-misty.RegisterUserEvent("sleep", true);
-function _sleep(data) {
+misty.RegisterUserEvent("emotion_sleep", true);
+function _emotion_sleep(data) {
 
     
     misty.DisplayImage("e_DefaultContent.jpg");
@@ -76,8 +78,8 @@ function _sleep(data) {
 } 
 
 
-misty.RegisterUserEvent("activitysuggestion", true);
-function _activitysuggestion(data) {
+misty.RegisterUserEvent("emotion_activitysuggestion", true);
+function _emotion_activitysuggestion(data) {
 
     
     misty.DisplayImage("e_DefaultContent.jpg");
@@ -86,3 +88,60 @@ function _activitysuggestion(data) {
     misty.Pause(4000)
     misty.MoveArmsDegrees(0, 0, 100, 100);
 } 
+
+misty.RegisterUserEvent("emotion_dormi", true);
+
+// TimerEvent callback
+function _emotion_dormi(data) {
+    misty.DisplayImage("e_SleepingZZZ.jpg");
+    misty.MoveHead(40, 0, 0, 100);
+ 
+}
+
+
+misty.RegisterUserEvent("emotion_sveglia", true);
+
+// TimerEvent callback
+function _emotion_sveglia(data) {
+    misty.DisplayImage("e_DefaultContent.jpg");
+    misty.MoveHead(0, 0, 0, 100);
+ 
+}
+
+
+misty.RegisterUserEvent("emotion_happy", true);
+
+// TimerEvent callback
+function _emotion_happy(data) {
+    misty.DisplayImage("e_Joy2.jpg");
+    misty.Pause(4000)
+    misty.DisplayImage("e_DefaultContent.jpg");
+}
+
+
+misty.RegisterUserEvent("emotion_sad", true);
+
+// TimerEvent callback
+function _emotion_happy(data) {
+    misty.DisplayImage("e_Terror.jpg");
+    misty.Pause(4000)
+    misty.DisplayImage("e_DefaultContent.jpg");
+}
+
+misty.RegisterUserEvent("emotion_ecstacy", true);
+
+// TimerEvent callback
+function _emotion_ecstacy(data) {
+    misty.DisplayImage("e_EcstacyHilarious.jpg");
+    misty.Pause(4000)
+    misty.DisplayImage("e_DefaultContent.jpg");
+}
+
+misty.RegisterUserEvent("emotion_sadness", true);
+
+// TimerEvent callback
+function _emotion_sadness(data) {
+    misty.DisplayImage("e_Sadness.jpg");
+    misty.Pause(4000)
+    misty.DisplayImage("e_DefaultContent.jpg");
+}
